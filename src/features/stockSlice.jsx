bcb64f8dc68925d3f9555d768042f87033a4237c
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2022-11-22 18:39:08 +0300

    first commit

---
+++ src/features/stockSlice.jsx
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const stockSlice = createSlice({
+  name: "stock",
+
+  initialState: {
+    purchases:null,
+    sales:null,
+    products:null,
+    brands:null,
+    firms:null,
+    categories:null,
+    loading: false,
+    error: false,
+  },
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    getSuccess: (state, { payload:{data,url} }) => {
+      state.loading = false;
+      state[url]=data
+    },
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+});
+
+export const { fetchStart, loginSuccess, logoutSuccess, registerSuccess, fetchFail } =
+  stockSlice.actions;
+export default stockSlice.reducer;
