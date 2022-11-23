ee6e2cba45d74d2ce1564105ef6dc346a22e0695
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2022-11-23 22:50:39 +0300

    commit

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,15 +3,20 @@ import useStockCalls from "../hooks/useStockCalls";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { useSelector } from "react-redux";
-import FirmCard from "../components/FirmCard";
-import FirmModal from "../components/modals/FirmModal";
-import { getSnackbarUnstyledUtilityClass } from "@mui/base";
+import { btnHoverStyle } from "../styles/globalStyle";
 
 const Firms = () => {
   const { getBrands, getCategories, getProducts } = useStockCalls();
-  const { firms } = useSelector((state) => state.stock);
+  const { products } = useSelector((state) => state.stock);
   const [open, setOpen] = useState(false);
   const [info, setInfo] = useState({});
 
@@ -33,14 +38,40 @@ const Firms = () => {
       {/*
       <ProductModal open={open} setOpen={setOpen} info={info} setInfo={setInfo} /> */}
 
-      {firms?.length > 0 && (
-        <Grid container justifyContent="center" gap={3}>
-          {firms?.map((firm) => (
-            <Grid item key={firm.id}>
-              <FirmCard firm={firm} setOpen={setOpen} setInfo={setInfo} />
-            </Grid>
-          ))}
-        </Grid>
+      {products?.length > 0 && (
+        <TableContainer component={Paper} sx={{ mt: 3 }} elevation={10}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">#</TableCell>
+                <TableCell align="center">Category</TableCell>
+                <TableCell align="center">Brand</TableCell>
+                <TableCell align="center">Name</TableCell>
+                <TableCell align="center">Stock</TableCell>
+                <TableCell align="center">Operation</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {products.map((product, index) => (
+                <TableRow
+                  key={product.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell align="center" component="th" scope="row">
+                    {index + 1}
+                  </TableCell>
+                  <TableCell align="center">{product.category}</TableCell>
+                  <TableCell align="center">{product.brand}</TableCell>
+                  <TableCell align="center">{product.name}</TableCell>
+                  <TableCell align="center">{product.stock}</TableCell>
+                  <TableCell align="center">
+                    <DeleteIcon sx={btnHoverStyle} />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       )}
     </Box>
   );
