7bdffd8057b2783c009d087014618ca8ec154632
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2022-11-23 20:15:00 +0300

    commit

--- src/components/FirmCard.jsx
+++ src/components/FirmCard.jsx
@@ -3,35 +3,46 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import EditIcon from "@mui/icons-material/Edit";
 import { btnHoverStyle } from "../styles/globalStyle";
 import useStockCalls from "../hooks/useStockCalls";
+import { CardHeader } from "@mui/material";
 
-
-export default function FirmCard({firm}) {
-    const {deleteFirm}=useStockCalls()
+export default function FirmCard({ firm }) {
+  const { deleteFirm } = useStockCalls();
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card
+      sx={{
+        p: 2,
+        maxWidth: "300px",
+        maxHeight: "400px",
+        minHeight: "400px",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <CardHeader title={firm?.name} subheader={firm?.address} />
       <CardMedia
-        component="img"
-        alt="firm-image"
-        height="140"
+        height="325"
+        width="250"
         image={firm?.image}
+        sx={{ p: 1, objectFit: "contain" }}
+        component="img"
+        alt="firm-img"
       />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {firm?.phone}
-        </Typography>
         <Typography variant="body2" color="text.secondary">
-          {firm?.address}
+          Phone: {firm?.phone}
         </Typography>
       </CardContent>
       <CardActions sx={{ display: "flex", justifyContent: "center" }}>
         <EditIcon sx={btnHoverStyle} />
-        <DeleteOutlineIcon sx={btnHoverStyle} onClick={() => deleteFirm(firm?.id)} />
+        <DeleteOutlineIcon
+          sx={btnHoverStyle}
+          onClick={() => deleteFirm(firm?.id)}
+        />
       </CardActions>
     </Card>
   );
