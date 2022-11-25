50440d4943d9c7f6bb5af86778a20104770063bb
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2022-11-25 21:35:34 +0300

    commit

--- src/components/KpiCards.jsx
+++ src/components/KpiCards.jsx
@@ -1,51 +1,67 @@
-import { Avatar, Box, Grid, Paper, Typography } from '@mui/material'
+import { Grid, Paper, Typography, Avatar, Box } from "@mui/material";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PaymentsIcon from "@mui/icons-material/Payments";
-import { amber, indigo, pink } from '@mui/material/colors';
-import { flexCenter } from '../styles/globalStyle';
-import { useSelector } from 'react-redux';
+import { indigo, pink, amber } from "@mui/material/colors";
+import { flex } from "../styles/globalStyle";
+import { useSelector } from "react-redux";
 
 const KpiCards = () => {
-    const {sales,purchases}=useSelector((state)=>state.stock)
+  const { sales, purchases } = useSelector((state) => state.stock);
 
-    const totalSales=sales?.
+  const totalSales = sales?.map((sale)=>Number(sale.price_total)).reduce((acc,val)=>acc+val,0)
+
+  const totalPurchases = purchases
+    ?.map((purchases) => Number(purchases.price_total))
+    .reduce((acc, val) => acc + val, 0);
+
+    const totalProfit = totalSales - totalPurchases;
+
+  console.log(totalSales)
 
   const data = [
     {
       title: "sales",
-      metric: "$0",
-      icon: <MonetizationOnIcon />,
+      metric: `$${totalSales || 0}`,
+      icon: <MonetizationOnIcon sx={{ fontSize: "3rem" }} />,
       color: indigo[900],
       bgColor: indigo[100],
     },
     {
       title: "profit",
-      metric: "$0",
-      icon: <PaymentsIcon />,
+      metric: `$${totalProfit || 0}`,
+      icon: <PaymentsIcon sx={{ fontSize: "3rem" }} />,
       color: pink[900],
       bgColor: pink[100],
     },
     {
       title: "purchases",
-      metric: "$0",
-      icon: <ShoppingCartIcon />,
+      metric: `$${totalPurchases || 0}`,
+      icon: <ShoppingCartIcon sx={{ fontSize: "3rem" }} />,
       color: amber[900],
       bgColor: amber[100],
     },
   ];
 
-
   return (
     <Grid container justifyContent="center" alignItems="center" spacing={2}>
       {data.map((item) => (
-        <Grid item key={item.title} xs={12} sm={6} md={4}>
-          <Paper sx={{p:2}} elevation={10}>
-            <Box sx={{display:"flex", justifyContent:"space-around"}}>
-              <Avatar sx={{width:"4rem", height:"4rem",color:item.color,backgroundColor:item.bgColor}}>{item.icon}</Avatar>
-              <Box>
+        <Grid item key={item.title} sx={{ width: "400px" }}>
+          <Paper sx={{ p: 2 }} elevation={10}>
+            <Box sx={{ display: "flex" }}>
+              <Avatar
+                sx={{
+                  width: "4rem",
+                  height: "4rem",
+                  color: item.color,
+                  backgroundColor: item.bgColor,
+                }}
+              >
+                {item.icon}
+              </Avatar>
+              <Box sx={{ mx: 4, flexGrow: 1 }}>
                 <Typography variant="button">{item.title}</Typography>
-                <Typography variant="h5">{item.metric}</Typography>
+                <Typography variant="h4">{item.metric}</Typography>
               </Box>
             </Box>
           </Paper>
@@ -53,6 +69,6 @@ const KpiCards = () => {
       ))}
     </Grid>
   );
-}
+};
 
-export default KpiCards
+export default KpiCards;
